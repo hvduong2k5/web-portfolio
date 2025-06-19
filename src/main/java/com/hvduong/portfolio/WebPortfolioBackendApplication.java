8ec4b0d4f0a16338ac1b7e7df2a7751239426a99
Hoang Van Duong <hd2005555@gmail.com>
2025-06-19 15:40:33 +0700

    init project

---
+++ src/main/java/com/hvduong/portfolio/WebPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hvduong.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPortfolioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hvduong/portfolio/WebPortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvduong.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
